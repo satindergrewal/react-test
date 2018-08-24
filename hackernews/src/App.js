a0fcba5b3a5e5648211ed542f950c40337586aaa
satindergrewal <grewal@lavabit.com>
2018-08-25 00:00:23 +1200

    fetching data

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -6,24 +6,11 @@ import './App.css';
 		Table
 	} from 'reactstrap';*/
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 function isSearched(searchTerm) {
 	return function (item) {
@@ -36,12 +23,25 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			list: list,
-			searchTerm: '',
+			result: null,
+			searchTerm: DEFAULT_QUERY,
 		};
 
+		this.setSearchTopStories = this.setSearchTopStories.bind(this);
 		this.onSearchChange = this.onSearchChange.bind(this);
 		this.onDismiss = this.onDismiss.bind(this);
+	}
+	setSearchTopStories(result) {
+		this.setState({ result });
+	}
+
+	componentDidMount() {
+		const { searchTerm } = this.state;
+
+		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+			.then(response => response.json())
+			.then(result => this.setSearchTopStories(result))
+			.catch(error => error);
 	}
 	onDismiss(id) {
 		const isNotId = item => item.objectID !== id;
@@ -53,7 +53,10 @@ class App extends Component {
 		this.setState({ searchTerm: event.target.value });
 	}
   render() {
-  	const { searchTerm, list } = this.state;
+  	const { searchTerm, result } = this.state;
+
+  	if (!result) { return null; }
+
     return (
 		<div className="page">
 			<div className="interactions">
@@ -65,7 +68,7 @@ class App extends Component {
 				</Search>
 			</div>
 			<Table
-				list={list}
+				list={result.hits}
 				pattern={searchTerm}
 				onDismiss={this.onDismiss}
 			/>
